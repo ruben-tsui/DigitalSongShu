fe96d1fe5bda3c3b49bb43e1ec4e3d064db6ce75
Ruben G. Tsui <37323290+ruben-tsui@users.noreply.github.com>
2019-03-19 08:11:05 +0800

    Add files via upload

---
+++ Surname.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 12 08:54:13 2019
+
+@author: ruben
+"""
+
+import re
+
+fin_surname_list = 'china_surnames.txt'
+
+class Surname:
+
+    def __init__(self):
+        with open(fin_surname_list, "r", encoding="utf-8") as fi:
+            surnames = fi.read().split()
+        #sort list by string length, descending order
+        surnames.sort(key = lambda s: len(s), reverse=True)
+        self.surname_list = surnames
+
+    def split_name(self, fullname):
+        for idx, sn in enumerate(self.surname_list):
+            m = re.search(fr"^{sn}", fullname)
+            if m:
+                return fullname[:m.end()], fullname[m.end():]
+        return None, None
+
+
